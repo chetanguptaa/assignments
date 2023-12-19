5c1356984613e94f6c186bbb4b95aa262dac0fd8
chetan gupta <chetangupta123raj@gmail.com>
2023-12-19 17:47:19 +0530

    02-jwt done

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,21 +1,37 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const jwtPassword = "secret";
+const z = require("zod");
+
+const signJwtSchema = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
 
 function signJwt(username, password) {
-
+  const validateInput = signJwtSchema.safeParse({ username, password });
+  if (validateInput.success) {
+    return jwt.sign({ username }, jwtPassword);
+  } else return null;
 }
 
 function verifyJwt(token) {
-
+  try {
+    jwt.verify(token, jwtPassword);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 function decodeJwt(token) {
-
+  return jwt.decode(token) != null;
 }
+
+console.log(decodeJwt("sdf"));
 
 module.exports = {
-    signJwt,
-    verifyJwt,
-    decodeJwt,
-    jwtPassword
-}
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
