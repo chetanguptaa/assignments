1df9fe35b5e651f0b38d443ee727562b15fe34fa
chetan gupta <chetangupta123raj@gmail.com>
2024-01-09 11:35:03 +0530

    feat: useMemo assignments done

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,22 +1,45 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
-// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
+// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input.
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
 
-export function Assignment1() {
-    const [input, setInput] = useState(0);
-    // Your solution starts here
-    const expensiveValue = 0; 
-    // Your solution ends here
+function findFactorial(n, map = new Map()) {
+  if (n === 0 || n === 1) return 1;
+  if (map.has(n)) return map.get(n);
 
-    return (
-        <div>
-            <input 
-                type="number" 
-                value={input} 
-                onChange={(e) => setInput(Number(e.target.value))} 
-            />
-            <p>Calculated Value: {expensiveValue}</p>
-        </div>
-    );
+  map.set(n, n * findFactorial(n - 1));
+  return map.get(n);
 }
+
+function Assignment1() {
+  const [input, setInput] = useState(0);
+  const [counter, setCounter] = useState(0);
+  // Your solution starts here
+
+  const expensiveValue = useMemo(() => {
+    console.log("Hi there");
+    findFactorial(input);
+  }, [input]);
+
+  // Your solution ends here
+
+  return (
+    <div>
+      <div>
+        <input
+          type="number"
+          value={input}
+          onChange={(e) => setInput(Number(e.target.value))}
+        />
+        <p>Calculated Value: {expensiveValue}</p>
+      </div>
+      <div>
+        <button onClick={() => setCounter(counter + 1)}>
+          counter {counter}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Assignment1;
--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -1,29 +1,42 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from "react";
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
 const Assignment3 = () => {
-    const [items, setItems] = useState([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
-        // Add more items as needed
-    ]);
+  const [items, setItems] = useState([
+    { name: "Chocolates", value: 10 },
+    { name: "Chips", value: 20 },
+    { name: "Onion", value: 30 },
+    { name: "Tomato", value: 30 },
+    // Add more items as needed
+  ]);
 
-    // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
-    return (
-        <div>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
-                ))}
-            </ul>
-            <p>Total Value: {totalValue}</p>
-        </div>
-    );
+  // Your code starts here
+
+  const [counter, setCounter] = useState(0);
+
+  const totalValue = useMemo(() => {
+    let total = 0;
+    for (let i = 0; i < items.length; i++) total += items[i].value;
+    return total;
+  }, [items]);
+
+  // Your code ends here
+  return (
+    <div>
+      <div>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.name} - Price: ${item.value}
+            </li>
+          ))}
+        </ul>
+        <p>Total Value: {totalValue}</p>
+      </div>
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+    </div>
+  );
 };
 
 export default Assignment3;
